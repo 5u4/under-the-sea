de0e9375a5e75dbfda5d5616be23d42239edf06e
Sen Hung Wong <alexwongsenhung@gmail.com>
2019-04-08 00:22:57 -0700

    Rotate camera

--- js/graphic/camera.js
+++ js/graphic/camera.js
@@ -1,3 +1,9 @@
 const _ratio = window.innerWidth / window.innerHeight;
 const camera = new THREE.PerspectiveCamera(75, _ratio, 0.1, 1000);
 camera.position.set(0, 1, 3);
+
+addEventListener("mousemove", e => {
+  const x = e.clientX / window.innerWidth - 0.5;
+  const y = e.clientY / window.innerHeight - 1;
+  camera.lookAt(x, -y, 0);
+});
