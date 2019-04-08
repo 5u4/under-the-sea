afee9f6ba4dfac04af3f9a74ff2af0784bf3f794
Sen Hung Wong <alexwongsenhung@gmail.com>
2019-04-08 01:52:17 -0700

    Make random

---
+++ js/util/random.js
@@ -0,0 +1,7 @@
+const randInt = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const randFloat = (min, max) => {
+  return Math.random() * (max - min + 1) + min;
+};
--- models/blueTang/blueTang.js
+++ models/blueTang/blueTang.js
@@ -1,33 +1,38 @@
 const loadBlueTang = async () => {
+  const flipped = randInt(0, 1) === 0 ? 1 : -1;
+
+  const position = [flipped * 10, randFloat(0.5, 5), randFloat(-5, 0)];
+  const scale = [0.1, 0.1, 0.1];
+  const rotation = [
+    -90 * THREE.Math.DEG2RAD,
+    0 * THREE.Math.DEG2RAD,
+    -flipped * 90 * THREE.Math.DEG2RAD
+  ];
+
   const blueTang = await loadModel(
     "models/blueTang/blueTang.jpg",
     "models/blueTang/blueTang.obj"
   );
 
-  const scale = 0.1;
-  blueTang.position.set(10, 2, -4);
-  blueTang.scale.set(scale, scale, scale);
-  blueTang.rotation.set(
-    -90 * THREE.Math.DEG2RAD,
-    0 * THREE.Math.DEG2RAD,
-    -90 * THREE.Math.DEG2RAD
-  );
+  blueTang.position.set(...position);
+  blueTang.scale.set(...scale);
+  blueTang.rotation.set(...rotation);
   scene.add(blueTang);
 
-  let speed = 0.03;
   const boarder = 11;
-  const originalHeight = 2;
+  const speed = flipped * 0.03 * randFloat(0.8, 1.2);
+  const wave = 0.3 * randFloat(0.8, 1.2);
 
   const animation = () => {
-    if (Math.abs(blueTang.position.x) <= boarder) {
-      blueTang.position.x -= speed;
-      blueTang.position.y =
-        originalHeight + Math.sin(performance.now() / 1000) * 0.4;
-    } else {
-      speed = -speed;
-      blueTang.rotation.z = -blueTang.rotation.z;
-      blueTang.position.x -= speed;
+    if (Math.abs(blueTang.position.x) > boarder) {
+      scene.remove(blueTang);
+      delete blueTang;
+      return;
     }
+
+    blueTang.position.x -= speed;
+    blueTang.position.y =
+      position[1] + Math.sin(performance.now() / 1000) * wave;
   };
 
   animations.push(animation);
